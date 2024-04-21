398106f79c35891bb23f2f2381dcf4567b66cee0
songjungah <1104wjddk@naver.com>
2024-04-21 11:26:11 +0900

    240421

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+// index 이름은 폴더까지만 import 해도  됨
+import Modal from './components/modal-popup/modal';
+import { useState } from 'react';
+
+// state 를 만들어서 모달창을 띄우고 지우고
+
+
+function App() {
+  let [showModal, setShowModal] = useState(false); // true면 모달뜨고 false면 안뜨고
+
+  function modalClose(){
+    // 모달을 열기 위해선 setShowModal(true) 했으니깐
+    // 모달을 닫기 위해선 ~~~~~
+    // 리액트에서 화면을 변경하기 위해선 state 를 수정한다
+    setShowModal(false);
+  }
+
+
+  // 버튼 누르면 setShowModal 을 true로 바꾼다. (showModal을 true로 변경하게끔)
+  return (
+    <div className="App">
+      <button onClick={()=>{setShowModal(true)}}>모달창 열기</button>
+      {
+        // showModal 이 true일때만 <Modal /> 실행하게끔 코딩
+        // && : 그리고 (앞과 뒤가 맞으면 true, 하나라도 틀리면 false)
+        // && : 앞에꺼가 틀리면 (false, null, Nan) 뒤에꺼는 어차피 검사안함
+        showModal && <Modal id={'modal-1'} onClose={modalClose} _body={<div>모달창 바디</div>}
+        _header={<div><p>Title</p></div>} _footer={<h2>Footer</h2>}
+        />
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/modal-popup/modal_강사님.jsx
@@ -0,0 +1,42 @@
+import './modal.css';
+
+// Modal 컴포넌트 만들어서 App.js 에서 사용
+// 컴포넌트 만든다음 내보낸다 (컴포넌트는 대문자로 시작하는 함수)
+export default function Modal({id, _header, _body, _footer, onClose})
+{
+  return(
+    <>
+    {/* id값은 사용하는 곳에서 설정할 수 있게 해보자 
+    ==> 매개변수 (사용하는 곳에서 결정하게한다)
+    ==> 컴포넌트의 매개변수 1. props 2. {} 직접생성
+
+    id값을 사용하는 곳에서 넣어줬으면 그것으로 설정하고,
+    안넣었으면 'Modal' 로 아이디 설정
+    && : (그리고)앞에꺼가 틀렸으면 어차피 틀린거여서 뒤에꺼 무시
+    || : (또는)앞에꺼가 맞았으면 어차피 맞은거여서 뒤에꺼 무시
+    틀렸다(0, false, "", null, NaN, undefined)
+    */}
+      <div className='modal-component' id={id || 'Modal'} onClick={(e)=>{
+        //console.log(e.target.className)
+        if(e.target.className === 'modal-component'){
+          onClose();
+        }
+      }}>
+        <div className='modal-content'>
+          <div className='modal-header'>
+            <span className='close-modal-icon' onClick={onClose}>&times;</span>
+            {/* 모달의 내용은 사용하는 곳에서 정하자 == 매개변수 */}
+            <h2>{_header}</h2>
+          </div>
+          <div className='modal-body'>
+            {_body}
+          </div>
+          <div className='modal-footer'>
+            {_footer}
+          </div>
+
+        </div>
+      </div>
+    </>
+  )
+}
